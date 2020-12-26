23770b3e5aa9693bf8fda22220805d93300d87f5
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-26 18:21:40 +0700

    Create InputReader.cs

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="InputReader", menuName ="Input/InputReader")]
+public class InputReader : ScriptableObject
+{
+    private GameInput gameInput;
+
+    private void OnEnable()
+    {
+        if(gameInput == null)
+        {
+            gameInput = new GameInput();
+        }
+    }
+}
