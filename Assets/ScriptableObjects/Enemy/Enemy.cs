90fa79ea0f8d9fa37c43b8458a1893c8e48d8074
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-28 00:03:37 +0700

    Create attack prototype

---
+++ Assets/ScriptableObjects/Enemy/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Data
+    private int health = 100; 
+
+    public void TakeDamage(int damage)
+    {
+        Debug.Log("Taking damage " + damage);
+        health -= damage;
+    }
+}
