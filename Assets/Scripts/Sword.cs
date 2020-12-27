684aa5a66989f3da5e76cad92f12074439e4a7ff
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-28 00:03:37 +0700

    Create attack prototype

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Sword : MonoBehaviour
+{
+    [SerializeField] private GameObject _user;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Enemy>().TakeDamage(1);
+        }
+    } 
+}
