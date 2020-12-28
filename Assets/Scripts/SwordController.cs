b0bafb469f71be47efadf0144602f5fd372fefa2
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-28 19:28:15 +0700

    Now using trigger instead of collision

---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{ 
+    [SerializeField] private Sword[] _swords; 
+
+    public void AttackStart()
+    { 
+        foreach(Sword sword in _swords)
+            sword.Enable();
+    }
+
+    public void AttackEnded()
+    { 
+        foreach(Sword sword in _swords)
+            sword.Disable();
+    }
+}
