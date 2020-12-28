12e1934cd03b031e8ba39460150a13a55a6b1ab7
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-29 00:05:42 +0700

    Refactor and rename Sword to DamageDealer

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class DamageDealer : IDamageDealer
+{ 
+    [SerializeField] private DamageDealerTrigger[] _triggers; 
+
+    public void DealDamageStart()
+    { 
+        foreach(DamageDealerTrigger trigger in _triggers)
+            trigger.Enable();
+    }
+
+    public void DealDamageEnded()
+    { 
+        foreach (DamageDealerTrigger trigger in _triggers)
+            trigger.Disable();
+    }
+}
+
+public interface IDamageDealer
+{
+    void DealDamageStart();
+    void DealDamageEnded();
+}
