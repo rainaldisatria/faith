2326108b30759e6c253dc6effe8be96a8d7e848c
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-28 17:07:02 +0700

    Implement enemy back to home, chase, attack

--- Assets/Scripts/Battle/Enemy/StateMachines/Actions/SetChasingActionSO.cs
+++ Assets/Scripts/Battle/Enemy/StateMachines/Actions/SetChasingActionSO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Enemy_SetChasing", menuName = "State Machines/Actions/Enemy_Set Chasing")]
-public class SetChasingActionSO : StateActionSO<SetChasingToTrueAction>
+public class BackToHome : StateActionSO<SetChasingToTrueAction>
 {
     public StateAction.SpecificMoment moment;
     public bool value;
@@ -12,24 +12,12 @@ public class SetChasingActionSO : StateActionSO<SetChasingToTrueAction>
 public class SetChasingToTrueAction : StateAction
 {
     private EnemyBattler _enemyBattler;
-    private SetChasingActionSO _originSO => (SetChasingActionSO)base.OriginSO;
+    private BackToHome _originSO => (BackToHome)base.OriginSO;
 
     public override void Awake(StateMachine stateMachine)
     {
         _enemyBattler = stateMachine.GetComponent<EnemyBattler>();
-    }
-
-    public override void OnStateEnter()
-    {
-        if(_originSO.moment == SpecificMoment.OnStateEnter)
-            _enemyBattler.isChasing = _originSO.value;
-    }
-
-    public override void OnStateExit()
-    {
-        if (_originSO.moment == SpecificMoment.OnStateExit)
-            _enemyBattler.isChasing = _originSO.value;
-    }
+    } 
 
     public override void OnUpdate() { }
 }
