eb314b26d41005de789453177c5cc2673e4b2ae4
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-29 00:39:48 +0700

    Refactor

--- Assets/Scripts/DamageDealerr.cs
+++ Assets/Scripts/DamageDealerr.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 
 public class DamageDealer : IDamageDealer
-{ 
-    [SerializeField] private DamageDealerTrigger[] _triggers; 
+{
+    [SerializeField] private DamageDealerTrigger[] _triggers;
+
+    public DamageDealer(DamageDealerTrigger[] triggers)
+    {
+        _triggers = triggers;
+    }
 
     public void DealDamageStart()
-    { 
-        foreach(DamageDealerTrigger trigger in _triggers)
+    {
+        foreach (DamageDealerTrigger trigger in _triggers)
             trigger.Enable();
     }
 
     public void DealDamageEnded()
-    { 
+    {
         foreach (DamageDealerTrigger trigger in _triggers)
             trigger.Disable();
     }
