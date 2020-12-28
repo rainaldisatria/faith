41ae0c4fd81d816019cbe9bb754a65de4303d6ef
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-28 13:38:31 +0700

    Fix radius check

--- Assets/Scripts/Battle/Battlers/EnemyBattler.cs
+++ Assets/Scripts/Battle/Battlers/EnemyBattler.cs
@@ -7,18 +7,33 @@ public class EnemyBattler : Battler
 {
     [SerializeField] private NavMeshAgent _navMeshAgent;
     public bool isChasing = false;
+    private Vector3 firstLocation;
 
     private GameObject player;
 
     private void Awake()
     {
+        _animators = GetComponentsInChildren<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
         _navMeshAgent = GetComponent<NavMeshAgent>();
-    } 
+
+        firstLocation = this.transform.position;
+    }
+
+    private void Update()
+    {
+        if (isChasing)
+        {
+            Chase(player.transform.position);
+        }
+        else
+        {
+            Chase(firstLocation);
+        }
+    }
 
     protected override void OnDeath()
-    {
-        Destroy(this.gameObject);   
+    { 
     }
 
     private void Chase(Vector3 target)
--- Assets/Scripts/Battle/Enemy/StateMachines/Conditions/PlayerBetweenRadiusConditionSO.cs
+++ Assets/Scripts/Battle/Enemy/StateMachines/Conditions/PlayerBetweenRadiusConditionSO.cs
@@ -3,24 +3,24 @@
 [CreateAssetMenu(fileName = "IsPlayerBetweenRadius", menuName = "State Machines/Conditions/Is Player Between Radius")]
 public class PlayerBetweenRadiusConditionSO : StateConditionSO<BetweenRadiusCondition>
 {
-    public float Radius = 2;
+    public float Radius = 15;
 }
 
 public class BetweenRadiusCondition : Condition
 {
     private GameObject _playerPos;
-    private Vector3 _currentPos;
+    private Transform _currentPos;
     private PlayerBetweenRadiusConditionSO _originSO => (PlayerBetweenRadiusConditionSO)base.OriginSO;
 
     public override void Awake(StateMachine stateMachine)
     {
         _playerPos = GameObject.FindGameObjectWithTag("Player");
-        _currentPos = stateMachine.transform.position;
+        _currentPos = stateMachine.transform;
     }
 
     protected override bool Statement()
     {
-        if (Vector3.Distance(_currentPos, _playerPos.transform.position) < _originSO.Radius)
+        if (Vector3.Distance(_currentPos.transform.position, _playerPos.transform.position) < _originSO.Radius)
         {
             return true;
         }
