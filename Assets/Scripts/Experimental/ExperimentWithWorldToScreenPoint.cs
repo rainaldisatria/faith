b5ebd43c2d282f50188dbc6c524ec615d6ffda29
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-29 19:29:29 +0700

    Fix healthbar offset

---
+++ Assets/Scripts/Experimental/ExperimentWithWorldToScreenPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExperimentWithWorldToScreenPoint : MonoBehaviour
+{
+    public Image image; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.transform.position = Camera.main.WorldToScreenPoint(this.transform.position);
+    }
+}
