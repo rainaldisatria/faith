74dc9e87838e729ffcb5f694e959e1ba5c60d030
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-30 07:33:42 +0700

    Halfway implemnting Skill System

---
+++ Assets/Scripts/Skills/SkillBaseSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SkillBaseSO : ScriptableObject, ISkills
+{
+    [SerializeField] protected string _skillName;
+    [SerializeField] protected GameObject _effect;
+    [SerializeField] protected float delay;
+
+    public string SkillName { get => _skillName;}
+
+    public abstract IEnumerator Execute(Transform posToExecute);
+}
