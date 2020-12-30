589b2edbbe2cd123fa73ac678d3ad40280795053
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-30 18:08:20 +0700

    Revert Changes: Delete SceneControlSystem

--- Assets/Scripts/SceneControlSystem/ChapterSystem/ChaptersData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName ="ChapterSystem/ChaptersData")]
-public class ChaptersData : ScriptableObject
-{  
-    public List<ChapterData> Chapters;
-}
--- Assets/Scripts/SceneControlSystem/ChapterSystem/DTO/ChapterData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ChapterSystem/ChapterData", fileName = "New Chapter Data")]
-public class ChapterData : ScriptableObject
-{ 
-    public List<LevelData> Levels; 
-}
