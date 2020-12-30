8eb3efafff108da0d283b53c99c4033091e94e4c
rainaldisatria <rainaldisatria212@gmail.com>
2020-12-30 18:08:20 +0700

    Revert Changes: Delete SceneControlSystem

--- Assets/Scripts/SceneControlSystem/LevelSystem/DTO/LevelEvent/WorldEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(menuName = "LevelSystem/WorldEvent")]
-public class WorldEvent : LevelEvent
-{
-    public string SceneName { get => _sceneName; }
-
-    [SerializeField] private string _sceneName;
-}
