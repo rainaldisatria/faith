ef9416af46dfc4d13009e16ee278b53bf166f389
rainaldisatria <rainaldisatria212@gmail.com>
2021-01-10 11:04:56 +0700

    Fix animation not playing correctly because broken SM

---
+++ Assets/Scripts/Battle/Battlers/StateMachines/Actions/UseSkillActionSO.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "UseSkill", menuName = "State Machines/Actions/Use Skill")]
+public class UseSkillActionSO : StateActionSO<UseSkillAction>
+{
+    
+}
+
+public class UseSkillAction : StateAction
+{
+    private Battler _battler;
+
+    public override void Awake(StateMachine stateMachine)
+    {
+        _battler = stateMachine.GetComponent<Battler>();
+    }
+
+    public override void OnStateEnter()
+    {
+        _battler.UseSkill();
+    }
+
+    public override void OnUpdate() { }
+}
