7da614a39fcbeddf6ba058e3e4068c57454974dd
rainaldisatria <rainaldisatria212@gmail.com>
2021-01-10 11:50:41 +0700

    Fix delayed hit animation when hitted while using skill

---
+++ Assets/Scripts/Battle/Battlers/StateMachines/Actions/SetIsUsingSkillActionSO.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "SetIsUsingSkill", menuName = "State Machines/Actions/SetIsUsingSkill_")]
+public class SetIsUsingSkillActionSO : StateActionSO<SetIsUsingSkillAction>
+{
+    public bool IsUsingSkill;
+}
+
+public class SetIsUsingSkillAction : StateAction
+{
+    private Battler _battler;
+    private SetIsUsingSkillActionSO _originSO => (SetIsUsingSkillActionSO)base.OriginSO;
+
+    public override void Awake(StateMachine stateMachine)
+    {
+        _battler = stateMachine.GetComponent<Battler>();
+    }
+
+    public override void OnStateEnter()
+    {
+        _battler.IsUsingSkill = _originSO.IsUsingSkill;
+    }
+
+    public override void OnUpdate() { }
+}
