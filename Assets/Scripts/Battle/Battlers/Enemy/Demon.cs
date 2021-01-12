553b290d27390f6719b1e334576e08e784c40204
rainaldisatria <rainaldisatria212@gmail.com>
2021-01-12 11:35:32 +0700

    Demon prep

---
+++ Assets/Scripts/Battle/Battlers/Enemy/Demon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demon : EnemyBattler
+{
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    } 
+}
