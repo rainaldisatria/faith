110e270f8a3208d29cf42d262b2d3d849a54ae33
rainaldisatria <rainaldisatria212@gmail.com>
2021-01-12 12:01:42 +0700

    Working Demon prefab

--- Assets/Scripts/Battle/Battlers/Enemy/Demon.cs
+++ Assets/Scripts/Battle/Battlers/Enemy/Demon.cs
@@ -6,6 +6,23 @@ public class Demon : EnemyBattler
 {
     public override void Attack()
     {
-        throw new System.NotImplementedException();
-    } 
+        StartCoroutine("StartAttack");
+    }
+
+    private IEnumerator StartAttack()
+    {
+        if (!IsAttacking)
+        {
+            IsAttacking = true;
+
+            yield return new WaitForSeconds(Random.Range(0f, 0.1f));
+
+            this.Animators.PlayAll((i) =>
+                this.Animators[i].CrossFade("Attack1", 0.25f, -1, 0));
+
+            yield return new WaitForSeconds(Random.Range(4f, 5));
+
+            IsAttacking = false;
+        }
+    }
 }
