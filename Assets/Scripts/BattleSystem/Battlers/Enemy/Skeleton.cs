fb766336e4fe7894f6159d29b500cfdf3211bfc4
rainaldisatria <rainaldisatria212@gmail.com>
2021-01-15 15:49:32 +0700

    More tweak

--- Assets/Scripts/BattleSystem/Battlers/Enemy/Skeleton.cs
+++ Assets/Scripts/BattleSystem/Battlers/Enemy/Skeleton.cs
@@ -25,9 +25,9 @@ namespace AnthaGames.Assets.Scripts.BattleSystem.Battlers.Enemies
                 this.Animators.PlayAll((i) =>
                     this.Animators[i].CrossFade("Attack1", 0.25f, -1, 0));
 
-                yield return new WaitForSeconds(Random.Range(0.3f, 0.7f));
+                yield return new WaitForSeconds(Random.Range(0.7f, 1.2f));
 
-                IsAttacking = false;
+                IsAttacking = false; 
             }
         }
 
